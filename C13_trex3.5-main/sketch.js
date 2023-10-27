0268d1d347c6bbca916b90a223b49fa2cd9b418e
luadl <145807670+luadl@users.noreply.github.com>
2023-10-27 17:43:05 -0300

    Adicionando Arquivos
    Ver qual o erro do jogo

---
+++ C13_trex3.5-main/sketch.js
@@ -0,0 +1,120 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloud, cloudsGroup, cloudImage;
+
+var obstaculo1;
+var obstaculo2;
+var obstaculo3;
+var obstaculo4;
+var obstaculo5;
+var obstaculo6;
+
+var score;
+
+
+var newImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  obstaculo1 = loadImage("obstacle1.png");
+  obstaculo2 = loadImage("obstacle2.png");
+  obstaculo3 = loadImage("obstacle3.png");
+  obstaculo4 = loadImage("obstacle4.png");
+  obstaculo5 = loadImage("obstacle5.png");
+  obstaculo6 = loadImage("obstacles6.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  // trex.addAnimation("collided",trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  console.log("Hello"+ 5)
+  score = 0;
+}
+
+function draw() {
+  background(180);
+  text("pontos: " + score, 500, 50);
+  score = score + Math.round(frameCount/60);
+  
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  
+  //gerar as nuvens
+  spawnClouds();
+
+  gerar_obstaculos();
+  
+  drawSprites();
+}
+
+function spawnClouds() {
+  //escreva o código aqui para gerar as nuvens
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(10,60))
+    cloud.scale = 0.4;
+    cloud.velocityX = -3;
+    
+    
+    //atribua tempo de vida à variável
+    cloud.lifetime = 50
+    
+    //ajuste a profundidade
+    cloud.depth = trex.depth
+    trex.depth = trex.depth + 1;
+    }
+}
+
+function gerar_obstaculos(){
+  if(frameCount % 60 === 0){
+    var obstaculo = createSprite(400, 165, 10, 40);
+    obstaculo.velocityX = -6;
+    var numero_aleatorio = Math.round(random(1, 6));
+    switch(numero_aleatorio){
+      case 1: obstaculo.addImage(obstaculo1);
+              break;
+      case 2: obstaculo.addImage(obstaculo2);
+              break;
+      case 3: obstaculo.addImage(obstaculo3);
+              break;
+      case 4: obstaculo.addImage(obstaculo4);
+              break;
+      case 5: obstaculo.addImage(obstaculo5);
+              break;
+      case 6: obstaculo.addImage(obstaculo6);
+              break;
+      default: break;
+    }
+    obstaculo.scale = 0.5;
+    obstaculo.lifetime = 300;
+  }
+}
